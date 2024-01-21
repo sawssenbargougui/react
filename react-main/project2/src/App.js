8376b9d88ffe7c516ee4f3efd0945369e1512801
sawssen bargougui <sawssen.bargougui@ipsas.tn>
2024-01-21 14:40:12 +0000

    comi7

--- react-main/project2/src/App.js
+++ react-main/project2/src/App.js
@@ -1,91 +1,72 @@
+import React, { useState } from 'react';
 import Todo from "./components/Todo";
 import Form from "./components/Form";
 import FilterButton from "./components/FilterButton";
-import { useState } from "react"; 
-import { nanoid } from "nanoid"; 
- 
+import { nanoid } from "nanoid";
 
+const FILTER_MAP = {
+  Toutes: () => true,
+  Actives: (task) => !task.completed,
+  Terminées: (task) => task.completed,
+};
+
+const FILTER_NAMES = Object.keys(FILTER_MAP);
 
 function App(props) {
-  const btnList = props.filters.map((filter) => <FilterButton id={filter.id} name={filter.name} key={filter.id} />);
-  const remTasks = props.tasks.filter((task) => !task.completed).length;
-  const completedTasks = props.tasks.filter((task) => task.completed).length;
-  const allTasks = props.tasks.length;
-  const [tasks, setTasks] = useState(props.tasks); 
+  const [tasks, setTasks] = useState(props.tasks);
   const [filter, setFilter] = useState("Toutes");
 
-  function FilterButton(props) { 
-    return ( 
-    <button 
-    type="button" 
-    className="btn toggle-btn" 
-    onClick={() => props.setFilter(props.name)}> 
-    <span className="visually-hidden">Montrer </span> 
-    <span>{props.name}</span> 
-    <span className="visually-hidden"> Tâches</span> 
-    </button> 
-    ); 
-   } 
-   
-     const FILTER_MAP = { 
-      Toutes: () => true, 
-      Actives: (task) => !task.completed, 
-      Terminées: (task) => task.completed, 
-     }; 
-     const FILTER_NAMES = Object.keys(FILTER_MAP); 
-     // vous devez utiliser name comme prop dans le composant FilterButton pour le texte du bouton
-      
-     const filterList = FILTER_NAMES.map((name) => ( 
-      <FilterButton 
-      key={name} 
-      name={name} 
-      setFilter={setFilter} 
-      /> 
-     ));
-     // vous devez utiliser name comme prop dans le composant FilterButton pour le texte du bouton
-     const taskList = tasks 
-     .filter(FILTER_MAP[filter]) 
-     .map((task) => ( 
-     <Todo 
-     id={task.id} 
-     name={task.name} 
-     completed={task.completed} 
-     key={task.id} 
-     toggleTaskCompleted={toggleTaskCompleted} 
-     deleteTask={deleteTask} 
-     editTask={editTask} 
-     /> 
-     ))
-    function addTask(name) {
-      const newTask = { id: `todo-${nanoid()}`, name, completed: false }; 
-      setTasks([...tasks, newTask]);
-     }
-     function deleteTask(id) {
-      const remainingTasks = tasks.filter((task) => id !== task.id);
-      setTasks(remainingTasks);
-     } 
-     function editTask(id, newName) { 
-      const editedTaskList = tasks.map((task) => { 
-      // if this task has the same ID as the edited task 
-      if (id === task.id) { 
-      // 
-      return { ...task, name: newName }; 
-      } 
-      return task; 
-      }); 
-      setTasks(editedTaskList); 
-     }  
-    
+  const btnList = props.filters.map((filter) => (
+    <FilterButton id={filter.id} name={filter.name} key={filter.id} />
+  ));
+
+  const remTasks = tasks.filter((task) => !task.completed).length;
+  const completedTasks = tasks.filter((task) => task.completed).length;
+  const allTasks = tasks.length;
+
+  function addTask(name) {
+    const newTask = { id: `todo-${nanoid()}`, name, completed: false };
+    setTasks([...tasks, newTask]);
+  }
+
+  function deleteTask(id) {
+    const remainingTasks = tasks.filter((task) => id !== task.id);
+    setTasks(remainingTasks);
+  }
+
+  function editTask(id, newName) {
+    const editedTaskList = tasks.map((task) => {
+      if (id === task.id) {
+        return { ...task, name: newName };
+      }
+      return task;
+    });
+    setTasks(editedTaskList);
+  }
+
+  const taskList = tasks
+    .filter(FILTER_MAP[filter])
+    .map((task) => (
+      <Todo
+        id={task.id}
+        name={task.name}
+        completed={task.completed}
+        key={task.id}
+        toggleTaskCompleted={toggleTaskCompleted}  // Assurez-vous que cette fonction est définie
+        deleteTask={deleteTask}
+        editTask={editTask}
+      />
+    ));
+
   return (
     <div className="todoapp stack-large">
       <h1>TodoMatic</h1>
       <Form addTask={addTask} />
       <div className="filters btn-group stack-exception">
         {btnList}
-        {filterList}
       </div>
       <table>
-      <thead>
+        <thead>
           <tr>
             <th>Tâches terminées</th>
             <th>Tâches restantes</th>
@@ -93,12 +74,12 @@ function App(props) {
           </tr>
         </thead>
         <tbody>
-      <tr>
-        <td>{completedTasks}</td>
-        <td>{remTasks}</td>
-        <td>{allTasks}</td>
-      </tr>
-      </tbody>
+          <tr>
+            <td>{completedTasks}</td>
+            <td>{remTasks}</td>
+            <td>{allTasks}</td>
+          </tr>
+        </tbody>
       </table>
 
       <ul
